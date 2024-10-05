ef102675fec7f2d30f5853c25c232da676eb02ca
ZenMuhamadX <muhamadzen669@gmail.com>
2024-10-05 09:33:49 +0700

    initialize

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    float x = 20.F;
+    int binIntTest = 0b0101000011101;
+    System.out.println(binIntTest);
+    System.out.println(x);
+    for (long i = 0; i < 999999999; i++) {
+      System.out.print(i);
+    }
+  }
+}
